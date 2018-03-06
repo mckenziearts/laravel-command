eb5fd82a067643241501bebbac1cbeea669d0fe0
Mckenzie Arts <monneylobe@gmail.com>
2018-03-06 17:15:27 +0100

    Finalization of LaravelHelperCommand

--- src/Console/Commands/LaravelHelperCommand.php
+++ src/Console/Commands/LaravelHelperCommand.php
@@ -2,9 +2,9 @@
 
 namespace Mckenziearts\LaravelCommand\Console\Commands;
 
-use Illuminate\Console\Command;
+use Illuminate\Console\GeneratorCommand;
 
-class LaravelHelperCommand extends Command
+class LaravelHelperCommand extends GeneratorCommand
 {
     /**
      * The console command name.
@@ -33,6 +33,70 @@ class LaravelHelperCommand extends Command
      * @var string
      */
     protected $type = 'Helper';
+
+    /**
+     * Get the stub file for the generator.
+     *
+     * @return string
+     */
+    protected function getStub()
+    {
+        return __DIR__.'/../Stubs/helper.stub';
+    }
+
+    /**
+     * Get the destination class path.
+     *
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function getPath($name)
+    {
+        $name = str_replace($this->laravel->getNamespace(), '', $name);
+        return $this->laravel['path'].'/'.str_replace('\\', '/', $name).'Helper.php';
+    }
+
+    /**
+     * Get the default namespace for the class.
+     *
+     * @param string $rootNamespace
+     *
+     * @return string
+     */
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace.'\Helpers';
+    }
+
+    /**
+     * Replace the table name for the given stub.
+     *
+     * @param string $stub
+     * @param string $name
+     *
+     * @return string
+     */
+    protected function replaceNameStrings(&$stub, $name)
+    {
+        $table = ltrim(strtolower(preg_replace('/[A-Z]/', '_$0', str_replace($this->getNamespace($name).'\\', '', $name))), '_');
+        $stub = str_replace('DummyTable', $table, $stub);
+        $stub = str_replace('dummy_class', strtolower(str_replace($this->getNamespace($name).'\\', '', $name)), $stub);
+        return $this;
+    }
+
+    /**
+     * Build the class with the given name.
+     *
+     * @param string $name
+     * @return string
+     * @throws \Illuminate\Contracts\Filesystem\FileNotFoundException
+     */
+    protected function buildClass($name)
+    {
+        $stub = $this->files->get($this->getStub());
+        return $this->replaceNamespace($stub, $name)->replaceNameStrings($stub, $name)->replaceClass($stub, $name);
+    }
 
     /**
      * Get the console command options.
