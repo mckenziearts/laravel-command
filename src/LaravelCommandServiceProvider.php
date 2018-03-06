cbe2ec055d288608434db766b7231a2e373cea5b
Mckenzie Arts <monneylobe@gmail.com>
2018-03-06 15:36:55 +0100

    Add LaravelHelperCommand to command list

--- src/LaravelCommandServiceProvider.php
+++ src/LaravelCommandServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Mckenziearts\LaravelCommand;
 
 use Illuminate\Support\ServiceProvider;
+use Mckenziearts\LaravelCommand\Console\Commands\LaravelHelperCommand;
 use Mckenziearts\LaravelCommand\Console\Commands\LaravelObserverCommand;
 use Mckenziearts\LaravelCommand\Console\Commands\LaravelRepositoryCommand;
 
@@ -12,6 +13,7 @@ class LaravelCommandServiceProvider extends ServiceProvider
     protected $commands = [
         LaravelObserverCommand::class,
         LaravelRepositoryCommand::class,
+        LaravelHelperCommand::class
     ];
 
     /**
